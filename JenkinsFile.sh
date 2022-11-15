498bd0adcbbcbe3fd30a689ec9916e978616a052
akshatch02 <40990589+akshatch02@users.noreply.github.com>
2022-11-15 14:54:41 +0530

    Update JenkinsFile.sh

--- JenkinsFile.sh
+++ JenkinsFile.sh
@@ -8,7 +8,18 @@ pipeline {
     }
     stage('hello') {
       steps {
+        # creates a venv folder that contains a copy of python3 packages to isolate any further changes in packages from the system installation
+        sh 'python3 -m venv venv' 
+         # tell the shell to use the created virtual environment
+        sh 'source venv/bin/activate'
+        # install requirements
+        sh 'pip3 install -r requirements.txt'
+        # run the script
         sh 'python3 mongo-insert.py'
+        # remove the venv
+        sh 'rm -rf venv'
+        # deactivate the virtual environment
+        sh 'deactivate'
       }
     }
   }
