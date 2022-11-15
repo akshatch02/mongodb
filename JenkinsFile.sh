4aefa3e81ee95d7ade8b185af3dd067b8043111c
akshatch02 <40990589+akshatch02@users.noreply.github.com>
2022-11-15 16:58:58 +0530

    Update JenkinsFile.sh

--- JenkinsFile.sh
+++ JenkinsFile.sh
@@ -1,4 +1,4 @@
-properties([parameters([choice(choices: ['Insert', 'Update'], description: 'Choose the operation:', name: 'operation')])])
+properties([parameters([choice(choices: ['insert', 'update'], description: 'Choose the operation:', name: 'operation')])])
 pipeline {
   agent any
   stages {
@@ -13,7 +13,7 @@ pipeline {
         sh 'python3 -m venv venv'
         sh 'source venv/bin/activate'
         sh 'pip3 install -r requirements.txt'
-        sh 'python3 mongo-insert.py'
+        sh 'python3 mongo-${params.operation}.py'
         sh 'rm -rf venv'
       }
     }
