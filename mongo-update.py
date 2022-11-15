17f25d2a4b65f227c83045d1601537b6c9824b25
akshatch02 <40990589+akshatch02@users.noreply.github.com>
2022-11-15 17:03:26 +0530

    Create mongo-update.py

---
+++ mongo-update.py
@@ -0,0 +1,13 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["test"]
+mycol = mydb["mycollection"]
+
+myquery = { "address": "Highway 37" }
+newvalues = { "$set": { "address": "Canyon 123" } }
+
+mycol.update(myquery, newvalues)
+
+for x in mycol.find():
+  print("Updated: ", x)
