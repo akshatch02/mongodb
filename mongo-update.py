ea461c0b4c22ac21b7204c70a2bdc2c1d44700c4
akshatch02 <40990589+akshatch02@users.noreply.github.com>
2022-11-16 14:10:39 +0530

    Update mongo-update.py

--- mongo-update.py
+++ mongo-update.py
@@ -4,8 +4,10 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["test"]
 mycol = mydb["mycollection"]
 
-myquery = { "address": "Highway 37" }
-newvalues = { "$set": { "address": "Canyon 123" } }
+with open('mongo-update-from.json') as f:
+   myquery = json.load(f)
+with open('mongo-update-to.json') as f:
+   newvalues = json.load(f)
 
 mycol.update_one(myquery, newvalues)
 
