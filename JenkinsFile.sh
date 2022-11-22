c2b38bbeda05dd79deb085cb94552eec8b21ef2d
akshatch02 <40990589+akshatch02@users.noreply.github.com>
2022-11-22 13:43:05 +0530

    Update JenkinsFile.sh

--- JenkinsFile.sh
+++ JenkinsFile.sh
@@ -1,6 +1,10 @@
 properties([parameters([choice(choices: ['insert', 'update'], description: 'Choose the operation:', name: 'operation')])])
 pipeline {
   agent any
+  enviroment {
+    DEV_USER = credentials('DEV_USER')
+    OPERATION = "${params.operation}"
+  }
   stages {
     stage('version') {
       steps {
@@ -10,7 +14,7 @@ pipeline {
     stage('hello') {
       steps {
       echo "heyyyyyyyy ${params.operation}"
-      OPERATION = "${params.operation}"
+      echo 'helloooo $DEV_USER_USR'
         sh 'python3 -m venv venv'
         sh 'source venv/bin/activate'
         sh 'pip3 install -r requirements.txt'
