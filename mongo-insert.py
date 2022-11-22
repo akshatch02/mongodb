c707c11ff374bc617305356e97d1ea0ee62d29c0
akshatch02 <40990589+akshatch02@users.noreply.github.com>
2022-11-22 16:35:38 +0530

    Update mongo-insert.py

--- mongo-insert.py
+++ mongo-insert.py
@@ -1,6 +1,13 @@
 import pymongo
 import json
+import sys
+from configparser import ConfigParser
 
+my_list=sys.argv
+for ele in my_list:
+   print(ele)
+configur = ConfigParser()
+print ("Server****** : ", configur.get('DEV','server'))
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["test"]
 mycol = mydb["mycollection"]
