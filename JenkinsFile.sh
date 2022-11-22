c7263b612386af61baea87f7554469155a99af91
akshatch02 <40990589+akshatch02@users.noreply.github.com>
2022-11-22 15:56:19 +0530

    Update JenkinsFile.sh

--- JenkinsFile.sh
+++ JenkinsFile.sh
@@ -1,9 +1,11 @@
 properties([parameters([choice(choices: ['insert', 'update'], description: 'Choose the operation:', name: 'operation')])])
+properties([parameters([choice(choices: ['DEV', 'PROD'], description: 'Choose the environment:', name: 'environment')])])
 pipeline {
   agent any
   environment {
-    DEV_USER = credentials('DEV_USER')
+    DEV_USER = credentials("${params.environment}_USER")
     OPERATION="${params.operation}"
+    ENVIRONMENT="${params.environment}"
   }
   stages {
     stage('version') {
@@ -13,12 +15,10 @@ pipeline {
     }
     stage('hello') {
       steps {
-      echo "heyyyy ${params.operation}"
-        sh '$OPERATION'
         sh 'python3 -m venv venv'
         sh 'source venv/bin/activate'
         sh 'pip3 install -r requirements.txt'
-        sh "python3 mongo-${OPERATION}.py ${DEV_USER_USR}"
+        sh "python3 mongo-${OPERATION}.py ${ENVIRONMENT} ${DEV_USER_USR}"
         sh 'rm -rf venv'
       }
     }
